3115399eb1333e8b26460f9644b90aad5200513b
Joabesv <joabevarjao123@gmail.com>
2022-11-27 23:33:48 -0300

    fix(refactor) - refactoring plugin implementation

--- src/plugins/sensible.ts
+++ src/plugins/sensible.ts
@@ -1,18 +1,15 @@
 import type { SensibleOptions } from '@fastify/sensible';
 import type { FastifyPluginAsync } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
-import { server } from '../server';
-
-const { log } = server;
 
 type SensiblePluginType = FastifyPluginAsync<SensibleOptions>;
 
 const sensiblePlugin: SensiblePluginType = async (fastify, opts) => {
   try {
     await fastify.register(import('@fastify/sensible'));
-    log.info('Successfully registered sensiblePlugin');
+    fastify.log.info('Successfully registered sensiblePlugin');
   } catch (err) {
-    log.error('Plugin: Sensible, error on register');
+    fastify.log.error('Plugin: Sensible, error on register');
   }
 };
 
