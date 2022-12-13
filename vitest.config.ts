083460969954e9af3ee850d9b2acdc70c245c8d8
Joabesv <joabevarjao123@gmail.com>
2022-12-13 15:38:02 -0300

    feat(vitest) - implement config for in-source testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
   test: {
     exclude: ['**/node_modules/**', '**/dist/**'],
     testTimeout: 20000,
